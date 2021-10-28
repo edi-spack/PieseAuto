9f1de61e929ebda470dbdff4b5445f0898cbcf23
Eduard Späck <edi.spack@gmail.com>
2021-10-28 18:51:18 +0300

    Work in progress

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class MainForm {
+    private JTextField searchTextField;
+    private JPanel rootPanel;
+    private JButton addButton;
+    private JTable table;
+    private JButton cartButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Piese Auto");
+        frame.setContentPane(new MainForm().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
