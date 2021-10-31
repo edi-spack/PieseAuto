17d9dd19b8aea8b3296b800aaa31e2923a2761a2
ArduBC <75089261+ArduBC@users.noreply.github.com>
2021-10-31 18:37:44 +0200

    Update AutoPart.java

--- src/AutoPart.java
+++ src/AutoPart.java
@@ -1,2 +1,57 @@
-public class AutoPart {
+public class AutoPart{
+    private String name;
+    private String brand;
+    private String model;
+    private String id;
+    private double price;
+    private int stock;
+
+    public AutoPart(String name, String brand, String model, String id, double price, int stock){
+
+    }
+
+    //name
+    private String getName(){
+        return name;
+    }
+    private void setName(){
+        this.name = name;
+    }
+
+    //brand
+    private String getBrand(){
+        return brand;
+    }
+    private void setBrand(){
+        this.brand = brand;
+    }
+
+    //model
+    private String getModel(){
+        return model;
+    }
+    private void setModel(){
+        this.model = model;
+    }
+
+    //id
+    private String getId(){
+        return id;
+    }
+
+    //price
+    private double getPrice(){
+        return price;
+    }
+    private void setPrice(){
+        this.price = price;
+    }
+
+    //stock
+    private int getStock(){
+        return stock;
+    }
+    private void setStock(){
+        this.stock = stock;
+    }
 }
