1b6fc45c4f7d457e3c6876797c2da4455e97f950
Eduard Späck <edi.spack@gmail.com>
2021-10-31 21:26:29 +0200

    Functie addRowToTable()

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 public class MainForm {
     private JTextField searchTextField;
@@ -15,15 +17,27 @@ public class MainForm {
         frame.pack();
         frame.setVisible(true);
 
+        table.setModel(new DefaultTableModel() {
+            final String[] columnNames = {"Id", "Name", "Brand", "Model", "Price", "Stock"};
+
+            @Override
+            public int getColumnCount() {
+                return columnNames.length;
+            }
+
+            @Override
+            public String getColumnName(int index) {
+                return columnNames[index];
+            }
+        });
+    }
+
+    private void addRowToTable(String name, String brand, String model, String id, double price, int stock) {
+        ((DefaultTableModel)table.getModel()).addRow(new String[]{id, name, brand, model, Double.toString(price), Integer.toString(stock)});
     }
 
     public static void main(String[] args) {
         //new Login();
-
-        JFrame frame = new JFrame("Piese Auto");
-        frame.setContentPane(new MainForm().rootPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        new MainForm();
     }
 }
