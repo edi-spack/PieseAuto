3bb6ac6a3f3d39415d632f7dd99412c6824b3a8b
Pascan Anamaria <92977911+Anamaria250999@users.noreply.github.com>
2021-11-01 22:51:56 +0200

    Cart

---
+++ Cart.java
@@ -0,0 +1,36 @@
+public class Cart(){
+    private static  Cart instance;
+    private static ArrayList<AutoPart> listaPiese;
+
+    private Cart(){
+
+    }
+
+    public static Cart getInstance(){
+
+    }
+
+    public  void addToCart(AutoPart piesa){
+
+    }
+
+    public void removeFromCart(AutoPart piesa){
+
+    }
+
+    public int getTotalProducts(){
+
+    }
+
+    public double getTotalPrice(){
+
+    }
+
+    public void clearAll(){
+
+    }
+
+    public void completeOrder(){
+
+    }
+}
