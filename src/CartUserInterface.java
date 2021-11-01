64ccf805e4f57819a7e6ec5bc4ecba4f6ee7bb59
Ovidiu Postoiu <postoiu.o@gmail.com>
2021-11-02 00:47:39 +0200

    Cart functionality

--- src/CartUserInterface.java
+++ src/CartUserInterface.java
@@ -1,7 +1,10 @@
 import Common.Cart;
+import Exceptions.DatabaseException;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class CartUserInterface {
@@ -13,8 +16,10 @@ public class CartUserInterface {
     private JTable productsTable;
     private static JFrame frame;
     private static Cart myCart = Cart.getInstance();
+    private MainForm form;
 
-    public CartUserInterface() {
+    public CartUserInterface(MainForm form) {
+        this.form = form;
         if (frame != null) {
             frame.dispose();
         }
@@ -28,6 +33,44 @@ public class CartUserInterface {
         productsTable.setModel(setMyModel());
 
 
+        backBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+
+
+        deleteBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                myCart.removeFromCart(productsTable.getSelectedRow());
+                productsTable.setModel(setMyModel());
+            }
+        });
+
+        placeOrderBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                try {
+                    myCart.completeOrder();
+                } catch (DatabaseException ex) {
+                    ex.printStackTrace();
+                }
+
+                int ans = JOptionPane.showConfirmDialog(null,"Plasati comanda?", "Confirmare comanda",
+                        JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+                if (ans == 0) {
+                    frame.dispose();
+                    JOptionPane.showMessageDialog(null, "Comanda inregistrata cu succes", "Confirmare",
+                            JOptionPane.INFORMATION_MESSAGE);
+                }
+
+                form.fillTable();
+            }
+        });
     }
 
     private DefaultTableModel setMyModel() {
