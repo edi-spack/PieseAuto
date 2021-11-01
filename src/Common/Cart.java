d79ec59eed7153e9da1c3666616150dbd2242899
Eduard Späck <edi.spack@gmail.com>
2021-11-01 22:11:21 +0200

    Implementare ICart

--- src/Common/Cart.java
+++ src/Common/Cart.java
@@ -2,39 +2,56 @@ package Common;
 
 import java.util.ArrayList;
 
-public class Cart(){
-    private static  Cart instance;
-    private static ArrayList<AutoPart> listaPiese;
+public class Cart implements ICart{
 
-    private Cart(){
+    private static Cart instance;
+    private ArrayList<AutoPart> partsList;
 
+    private Cart() {
+        partsList = new ArrayList<>();
     }
 
-    public static Cart getInstance(){
-
+    public static Cart getInstance() {
+        if(instance == null) {
+            instance = new Cart();
+        }
+        return instance;
     }
 
-    public  void addToCart(AutoPart piesa){
-
+    public void addToCart(AutoPart product) {
+        partsList.add(product);
     }
 
-    public void removeFromCart(AutoPart piesa){
-
+    public void removeFromCart(AutoPart product) {
+        partsList.remove(product);
     }
 
-    public int getTotalProducts(){
-
+    public int getTotalProducts() {
+        return partsList.size();
     }
 
-    public double getTotalPrice(){
+    public double getTotalPrice() {
+        double result = 0.0;
 
+        for (AutoPart part: partsList) {
+            result += part.getPrice();
+        }
+
+        return result;
     }
 
-    public void clearAll(){
-
+    public void completeOrder() {
+        partsList.clear();
     }
 
-    public void completeOrder(){
+    public ArrayList<String[]> getCartItems() {
+        ArrayList<String[]> result = new ArrayList<>();
 
+        for (AutoPart part: partsList) {
+            String[] temp = {part.getName(), part.getBrand(), part.getModel(), Double.toString(part.getPrice())};
+            result.add(temp);
+        }
+
+        return result;
     }
 }
