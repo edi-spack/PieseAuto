3c8abfda9bb1a0c294592844431af4116fb15c98
Ovidiu Postoiu <postoiu.o@gmail.com>
2021-11-01 18:24:18 +0200

    Update SearchField

--- src/MainForm.java
+++ src/MainForm.java
@@ -3,14 +3,21 @@ import Database.SQLiteInterface;
 import Exceptions.DatabaseException;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Vector;
 
 public class MainForm {
     private JTextField searchTextField;
     private JPanel rootPanel;
     private JButton addButton;
     private JTable table;
+    private TableRowSorter sorter;
     private JButton cartButton;
     private JFrame frame;
     private ArrayList<AutoPart> parts;
@@ -23,8 +30,7 @@ public class MainForm {
         frame.setVisible(true);
 
         parts = new ArrayList<>();
-
-        table.setModel(new DefaultTableModel() {
+        DefaultTableModel model = new DefaultTableModel() {
             final String[] columnNames = {"Id", "Name", "Brand", "Model", "Price", "Stock"};
 
             @Override
@@ -36,19 +42,46 @@ public class MainForm {
             public String getColumnName(int index) {
                 return columnNames[index];
             }
-        });
+        };
+
+        sorter = new TableRowSorter(model);
+        table.setModel(model);
+        table.setRowSorter(sorter);
 
         //addRowToTable("name", "brand", "model", "id", 5.38, 10);
 
         SQLiteInterface database = new SQLiteInterface();
-        //database.addPart("1110F/S", "Roata", "Opel", "Zafira", 100.00, 10);
         parts = database.getDatabase();
         fillTable(parts);
+
+        //search field
+        searchTextField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                search(searchTextField.getText());
+            }
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                search(searchTextField.getText());
+            }
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                search(searchTextField.getText());
+            }
+            public void search(String str) {
+                if (str.length() == 0) {
+                    sorter.setRowFilter(null);
+                } else {
+                    sorter.setRowFilter(RowFilter.regexFilter(str));
+                }
+            }
+        });
     }
 
     private void addRowToTable(String id, String name, String brand, String model, double price, int stock) {
         ((DefaultTableModel)table.getModel()).addRow(new String[]{id, name, brand, model, Double.toString(price), Integer.toString(stock)});
     }
+
 
     private void fillTable(ArrayList<AutoPart> partsList) {
         for (AutoPart part: parts) {
