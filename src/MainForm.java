0a84e02ea3186157321d2bd4d0ea89e8340d8961
Eduard Späck <edi.spack@gmail.com>
2021-11-01 21:11:00 +0200

    AddPart functionality added

--- src/MainForm.java
+++ src/MainForm.java
@@ -4,6 +4,8 @@ import Exceptions.DatabaseException;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class MainForm {
@@ -13,9 +15,11 @@ public class MainForm {
     private JTable table;
     private JButton cartButton;
     private JFrame frame;
+    private MainForm form;
     private ArrayList<AutoPart> parts;
 
-    public MainForm() throws DatabaseException {
+    public MainForm() {
+        form = this;
         frame = new JFrame("Piese Auto");
         frame.setContentPane(rootPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -38,20 +42,46 @@ public class MainForm {
             }
         });
 
-        SQLiteInterface database = new SQLiteInterface();
-        //database.addPart("1110F/S", "Roata", "Opel", "Zafira", 100.00, 10);
-        parts = database.getDatabase();
         fillTable();
+
+        // Listeners
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AddPart addPartForm = new AddPart(form);
+            }
+        });
+    }
+
+    public void addPart(String id, String name, String brand, String model, double price, int stock) {
+        try {
+            SQLiteInterface database = new SQLiteInterface();
+            database.addPart(id, name, brand, model, price, stock);
+            fillTable();
+        } catch (DatabaseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void fillTable() {
+        SQLiteInterface database = null;
+
+        try {
+            database = new SQLiteInterface();
+            parts = database.getDatabase();
+        } catch (DatabaseException e) {
+            e.printStackTrace();
+        }
+
+        ((DefaultTableModel)table.getModel()).setRowCount(0);
+
+        for (AutoPart part: parts) {
+            addRowToTable(part.getId(), part.getName(), part.getBrand(), part.getModel(), part.getPrice(), part.getStock());
+        }
     }
 
     private void addRowToTable(String id, String name, String brand, String model, double price, int stock) {
         ((DefaultTableModel)table.getModel()).addRow(new String[]{id, name, brand, model, Double.toString(price), Integer.toString(stock)});
-    }
-
-    private void fillTable() {
-        for (AutoPart part: parts) {
-            addRowToTable(part.getId(), part.getName(), part.getBrand(), part.getModel(), part.getPrice(), part.getStock());
-        }
     }
 
     public static void main(String[] args) throws DatabaseException {
